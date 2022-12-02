a03f248d4348f612f5c96b05fc8ef341b15088a7
“Rodolfo <vladcarmona6@gmail.com>
2022-12-01 23:37:35 -0600

    creating /tmp/rails-app to precompile assets when start the app

--- unicorn.sh
+++ unicorn.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+if [ ! -d "/tmprails-app" ]
+then
+  mkdir "/tmp/rails-app"
+fi
+
+rails db:migrate
+rails assets:precompile
+
 if [ "${RAILS_ENV}" = "development" ]
 then
   echo "Starting rails"
-  rails db:migrate
   rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'
 fi
 
 if [ "${RAILS_ENV}" = "production" ]
 then
   echo "Starting unicorn"
-  rails db:migrate
   rm -f /unicorn/pids/unicorn.pid && unicorn -c config/unicorn.rb -E production
 fi
