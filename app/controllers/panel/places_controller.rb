21279c5bf617a2d5054001be7b7d259e5bdd6070
“Rodolfo <vladcarmona6@gmail.com>
2023-06-20 23:04:44 -0600

    replacing "http" string with "https"

--- app/controllers/panel/places_controller.rb
+++ app/controllers/panel/places_controller.rb
@@ -13,17 +13,17 @@ class Panel::PlacesController  < Panel::ApplicationController
 
   def update
     if @place.update(item_params)
-      @place.quality_flavors = @place.quality_flavors.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.content_right = @place.content_right.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.harvest = @place.harvest.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.content_left = @place.content_left.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.scheme = @place.scheme.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.quality_flavors = @place.quality_flavors.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.content_right = @place.content_right.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.harvest = @place.harvest.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.content_left = @place.content_left.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.scheme = @place.scheme.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
       
-      @place.quality_flavors_eng = @place.quality_flavors_eng.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.content_right_eng = @place.content_right_eng.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.harvest_eng = @place.harvest_eng.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.content_left_eng = @place.content_left_eng.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
-      @place.scheme_eng = @place.scheme_eng.gsub('src="/ckeditor_assets/pictures/','src="http://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.quality_flavors_eng = @place.quality_flavors_eng.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.content_right_eng = @place.content_right_eng.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.harvest_eng = @place.harvest_eng.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.content_left_eng = @place.content_left_eng.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
+      @place.scheme_eng = @place.scheme_eng.gsub('src="/ckeditor_assets/pictures/','src="https://cocoaflavormap.cacaomovil.com/ckeditor_assets/pictures/')
 
       @place.save
       redirect_to "/panel/places/#{@place.id}/edit", notice: "Actualizado correctamente"
